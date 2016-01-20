1f7db50d264cf102a3bb06dbde4bb92f96cfcbff
Виктор <viktortat@gmail.com>
2016-01-20 23:59:09 +0200

    test2

---
+++ GitEx1Vs2015/GitEx1Vs2015/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitEx1Vs2015.Startup))]
+namespace GitEx1Vs2015
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
